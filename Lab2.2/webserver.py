69860fe82cbf027bb9e04dc19394c9f504f12eef
ramilius <moonfbwer@mail.ru>
2017-08-29 18:04:07 +0300

    Lab1.5

--- Lab2.2/webserver.py
+++ Lab2.2/webserver.py
@@ -18,7 +18,11 @@ def python_info():
 def hosts_configs():
     return jsonify(str(hosts.keys()))
 
-#@app.route('/config/hostname')
+@app.route('/config/hostname')
+def hosts_ipaddress():
+    return jsonify(str(ips.keys()))
+
+
 
 def clasificator(line):
     result = 0
@@ -26,21 +30,30 @@ def clasificator(line):
     if (bool(hostname_match)):
         hostname = hostname_match.group(1)
         return ("HOST", hostname)
+
+    ip_net_match = re.match(
+        ' ip address ([0-9]{1,3})\.?([0-9]{1,3})\.?([0-9]{1,3})\.?([0-9]{1,3})\ ([0-9]{1,3})\.?([0-9]{1,3})\.?([0-9]{1,3})\.?([0-9]{1,3})',
+        line)
+    if (bool(ip_net_match)):
+        ip_mask = ip_net_match.group(1, 2, 3, 4, 5, 6, 7, 8)
+        ip = ip_mask[0] + '.' + ip_mask[1] + '.' + ip_mask[2] + '.' + ip_mask[3]
+        return ("IP", ip)
+
     return ("UNCLASSIFIED",)
 
 if __name__ == '__main__':
-
     hosts = {}
-
-
+    ips = {}
     files = glob.glob("C:\\Users\\rusmanov.SUPPORT\\Seafile\\p4ne_training\\config_files\\*.txt")
     for j in files:
         with open(j) as f:
             for i in f:
                 a = clasificator(i)
                 if a[0] != "UNCLASSIFIED":
-                    hosts[a[1]] = a[1]
-
+                    if a[0] == "HOST":
+                        hosts[a[1]] = a[1]
+                    if a[0] == "IP":
+                        ips[a[1]] = a[1]
     app.run(debug = True)
 
 
